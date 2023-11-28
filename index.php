462dcc7cd3edfd1e2f8d27585dfb6030915ae1f1
Dipali-kalyankar <kalyankardipali@gmail.com>
2023-11-28 12:07:47 +0530

    first commit

--- index.php
+++ index.php
@@ -4,10 +4,25 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Select the  multiple images and upload in one folder</title>
+ <style>
+    body{
+        background-color: pink;
+    }
+    h1{
+        text-align: center;
+    }
+    h2{
+        text-align: center;
+    }
+    form{
+        text-align: center;
+    }
+    </style>
+
 </head>
 <body>
-    <h2>Upload the images</h2>
-    <p>select the file</p>
+    <h1>Upload the images</h1>
+    <h2>select the file</h2>
 
     <form action="upload.php" method="post"  enctype="multipart/form-data">
         <input type="file" name="myImages[]" multiple />
